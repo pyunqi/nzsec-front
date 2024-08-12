8618893f510fd86c87374dccae58d7ac8d36a387
pyunqi <pyunqi@gmail.com>
2024-08-13 01:21:10 +0800

    lazy loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client'
-import Image from "next/image";
 import dynamic from 'next/dynamic';
 import '../styles/styles.css'
 import Button from "@/components/Button";
-import Navbar from "@/components/Navbar";
+import React, { useEffect, useState } from 'react';
+import type { AppProps } from 'next/app';
+import LoadingSpinner from '../components/LoadingSpinner';
 // 动态导入 ThreeScene 组件，禁用服务器端渲染
 const ThreeScene = dynamic(() => import('../components/ThreeScene'), {
+  suspense: true,
   ssr: false, // 禁用服务器端渲染
 });
+
+
 export default function Home() {
   return (
     <>
