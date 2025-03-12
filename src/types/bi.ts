0821395999acc8b1bb22bda64c720382f6bfd5e3
pyunqi <pyunqi@gmail.com>
2025-03-12 19:34:33 +0800

    难道注释掉是不行的？

--- src/types/bi.ts
+++ src/types/bi.ts
@@ -1,16 +1,8 @@
-// type Author = {
-//   name: string;
-//   image: string;
-//   designation: string;
-// };
-
 export type BI = {
   id: number;
   title: string;
   paragraph: string;
   image: string;
-  // author: Author;
   tags: string[];
-  // publishDate: string;
   bid:string;
 };
